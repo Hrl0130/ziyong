b2e87ffc0c3f7cb00773f52ff756f066b94be3be
Hrl0130 <117156035+Hrl0130@users.noreply.github.com>
2022-11-01 18:33:51 +0800

    Create shufazidian.js

---
+++ shufazidian.js
@@ -0,0 +1,27 @@
+{
+  "rules" : [
+    {
+      "action" : "body",
+      "matchField" : "",
+      "field" : "",
+      "value" : "vip\":\"2099-09-09 00:23:00\"",
+      "matchValue" : "vip\":\".*?\"",
+      "destiontion" : "response",
+      "isRegex" : true
+    }
+  ],
+  "enabled" : false,
+  "name" : "书写字典",
+  "description" : "大神组测试",
+  "locations" : [
+    {
+      "method" : "",
+      "scheme" : "",
+      "enabled" : true,
+      "port" : 0,
+      "query" : "",
+      "host" : "wxapp-a.shufazidian.com",
+      "path" : "\/storm.php\/info"
+    }
+  ]
+}
